bc1b2a8f816eff7aaf9679bf3c5816447e2c9688
Nyssa <nyssagd@gmail.com>
2021-09-23 14:59:08 -0400

    "only understood the HashMap practice"

---
+++ src/exercises/ArrayListPractice.java
@@ -0,0 +1,19 @@
+//package exercises;
+//import java.util.Arrays;
+//import java.util.Scanner;
+//
+//public class ArrayListPractice {
+//    public static void main(String[] args) {
+//
+//
+//        public static int sumEven (ArrayList < Integer > arr) {
+//            int total = 0;
+//            for (int integer : arr) {
+//                if (integer % 2 == 0) {
+//                    total += integer;
+//                }
+//            }
+//            return total;
+//        }
+//    }
+//}
