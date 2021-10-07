96b3720cd1e8d253c0b7a42367e7d5069905274b
Nyssa <nyssagd@gmail.com>
2021-10-06 22:55:16 -0400

    "oh good. exciting cars."

--- src/org/launchcode/java/demos/lsn3classes1/Student.java
+++ src/org/launchcode/java/demos/lsn3classes1/Student.java
@@ -1,3 +1,4 @@
+
 package org.launchcode.java.demos.lsn3classes1;
 
 // Start working here with your Student class.
@@ -10,39 +11,39 @@ public class Student {
     private int numberOfCredits = 0;
     private double gpa = 0.0;
 
-
-
-public Student (String name, int studentId, int numberOfCredits, double gpa) {
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setStudentID(int studentId){
-        this.studentId = studentId;
-    }
-
-    public void setGpa(double gpa) {
-        this.gpa = gpa;
-    }
-
-    private void setNumberOfCredits(int numberOfCredits) {
-        this.numberOfCredits = numberOfCredits;
-    }
-
-    public String getName(){
-        return name;
-    }
-
-    public int getStudentId() {
-        return studentId;
-    }
-
-    public int getNumberOfCredits() {
-        return numberOfCredits;
-    }
-
-    public double getGpa(){
-        return gpa;
-    }
 }
-}
+
+//public Student (String name, int studentId, int numberOfCredits, double gpa) {
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public void setStudentID(int studentId){
+//        this.studentId = studentId;
+//    }
+//
+//    public void setGpa(double gpa) {
+//        this.gpa = gpa;
+//    }
+//
+//    private void setNumberOfCredits(int numberOfCredits) {
+//        this.numberOfCredits = numberOfCredits;
+//    }
+//
+//    public String getName(){
+//        return name;
+//    }
+//
+//    public int getStudentId() {
+//        return studentId;
+//    }
+//
+//    public int getNumberOfCredits() {
+//        return numberOfCredits;
+//    }
+//
+//    public double getGpa(){
+//        return gpa;
+//    }
+//}
+//}*/
--- src/org/launchcode/java/demos/lsn5unittesting/main/Car.java
+++ src/org/launchcode/java/demos/lsn5unittesting/main/Car.java
@@ -1,6 +1,12 @@
 package org.launchcode.java.demos.lsn5unittesting.main;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+
 
 public class Car {
+
+
 
     private String make;
     private String model;
@@ -47,6 +53,10 @@ public class Car {
     }
 
     public void setGasTankLevel(double gasTankLevel) {
+        if (gasTankLevel > this.getGasTankSize()) {
+            throw new IllegalArgumentException("Can't exceed tank size");
+        }
+
         this.gasTankLevel = gasTankLevel;
     }
 
@@ -69,6 +79,10 @@ public class Car {
      *
      * @param miles - the miles to drive
      */
+
+
+
+
     public void drive(double miles)
     {
         //adjust fuel based on mpg and miles requested to drive
@@ -83,6 +97,10 @@ public class Car {
         double gallonsUsed = milesAbleToTravel / this.milesPerGallon;
         this.gasTankLevel = this.gasTankLevel - gallonsUsed;
         this.odometer += milesAbleToTravel;
+    }
+
+    public void addGas(double gas) {
+        this.setGasTankLevel(gas + this.getGasTankLevel());
     }
 
 }
