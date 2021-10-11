2508405a28e46f6d9554064100b6f59b90a24f25
Nyssa <nyssagd@gmail.com>
2021-10-11 16:29:44 -0400

    "don't know how to extend abstract class."

---
+++ src/org/launchcode/java/demos/lsn6inheritance/Main.java
@@ -0,0 +1,8 @@
+package org.launchcode.java.demos.lsn6inheritance;
+
+//public class Main {
+//
+//    HouseCat garfield = new HouseCat ("Garfield",12);
+//    garfield.eat();
+//    System.out.println(garfield.isTired());
+//}
